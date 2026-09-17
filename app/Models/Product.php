76f4bc0c5ff1cf66da26cfc3f0aaee238ebe3990
GoldenFayv <favour.e2002@icloud.com>
2026-09-17 22:08:08 +0100

    fix: price returning in kobo

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+use function Pest\Laravel\get;
 
 #[Fillable(['name', 'slug', 'description', 'price', 'currency', 'type', 'is_active'])]
 class Product extends Model
@@ -21,4 +24,17 @@ class Product extends Model
     protected $casts = [
         'is_active' => 'boolean',
     ];
+
+    protected function Price(): Attribute
+    {
+        return Attribute::make(
+            // get: Closure::fromCallable([$this, 'toNaira'])
+            get: $this->toNaira(...)
+        );
+    }
+
+    private function toNaira(float $amount): float
+    {
+        return $amount / 100;
+    }
 }
